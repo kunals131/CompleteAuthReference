7bbabed03ce65bf3dbd512cc24477dc36b2e2511
kunals131 <86314140+kunals131@users.noreply.github.com>
2022-02-26 22:37:45 +0530

    Configured Redux & Tailwind in front-end

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import reducers from '../redux/reducers';
+import { applyMiddleware, createStore } from 'redux';
+
+
+const store = createStore(reducers)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+    <Component {...pageProps} />
+  </Provider>
 }
 
 export default MyApp
---
+++ client/redux/reducers/userReducer.js
@@ -0,0 +1,12 @@
+
+const userReducer = (state= {},action)=>{
+    switch(action.payload) {
+        case 'LOGIN_USER' : return action.payload;
+        case 'LOGOUT_USER' : return {};
+        case 'SET_USER' : return action.payload;
+        default : return state;
+    }
+
+}
+
+export default userReducer;
